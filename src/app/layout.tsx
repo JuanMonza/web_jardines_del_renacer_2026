370c5cce3c29a5acbf630fbca892f3d649949d7c
Juan Monza <juanmonza@icloud.com>
2026-08-14 14:36:38 -0500

    Integrar Google Tag Manager en todo el sitio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
+import Script from 'next/script';
 import AppShell from '@/components/layout/AppShell';
+
+const GOOGLE_TAG_MANAGER_ID = 'GTM-THR7J2P9';
 
 export const metadata: Metadata = {
   title: 'Jardines del Renacer - Dignidad y Paz Eterna',
@@ -26,7 +29,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" className={`${montserrat.variable}`}>
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');`}
+        </Script>
+      </head>
       <body className="antialiased min-h-screen flex flex-col font-sans">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GOOGLE_TAG_MANAGER_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <AppShell>{children}</AppShell>
       </body>
     </html>
